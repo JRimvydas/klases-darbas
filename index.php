28ee82888182cdf35bbd6f14ce7eaf44a7858ac9
JRimvydas <jucius.rim@gmail.com>
2020-02-03 11:12:50 +0200

    Antra uzduotis

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 $string = 'Šios dienos data - ';
 $date = date('yy/m/d') ;
 $result = $string . $date;
-print $result;
+print $result . "<br>";
+
+// Antra uzduotis
+// "2. Sukurti sakini is 5< zodziu.
+// po kiekvieno zodzio yra padetas taskas.
+// naudojant bazines php funkcijas tuos taskus pakeisti i tarpus."
+
+$sakinys = 'Labas. mano. vardas. yra. Rimvydas';
+$sakinys = str_replace(".", " ", $sakinys);
+print $sakinys;
 
  ?>
